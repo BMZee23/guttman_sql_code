87b7798fe66206ab0587b4dccc55f6b82c3e6404
doris quilizapa <dorisquili33@gmail.com>
2024-02-18 13:29:24 -0500

    create_users123.sql

--- Classic models.sql
+++ Classic models.sql
@@ -11,3 +11,9 @@ CREATE ROLE
 IF NOT EXISTS
      'READ_ONLY_CLASSICMODELS_DB', 'admin_user',
      'READ_ONLY_ EMPLOYEES_DB', 'app_user';
+
+     use DBName;
+     create login a_2 with password='Aa123';
+     create user a_2 for login a_2;
+     grant insert to a_2;
+     grant select to a_2;
